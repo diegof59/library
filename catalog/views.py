8722cb389a0cf8e961dc3a65490108db2f13a0cd
Diego Botero <diego.botero@correounivalle.edu.co>
2019-01-30 23:12:05 -0500

    Form buscar libros funcional.

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Libro
+
 def buscar_libro(request):
     if 'query' in request.GET:
-        if request.GET['query']:
-            mensaje = 'Estas buscando: %r' % request.GET['query']
+        query = request.GET['query']
+        if query:
+            libros = Libro.objects.filter(titulo__icontains = query)
+            context = {'libros': libros, 'query': query}
+            return render(request, 'catalog/libro_query.html', context)
         else:
-            mensaje = 'Haz subido un formulario vacio.'
+            context = {'error': True}
+            return render(request, 'catalog/buscar_libro.html', context)
         return HttpResponse(mensaje)
     else:
         return render(request, 'catalog/buscar_libro.html')
